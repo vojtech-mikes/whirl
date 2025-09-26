8bcc360bf39ee055a19828a9ef2303ad2828224c
Vojtech Mikes <vojtechmikes98@gmail.com>
2025-09-26 23:48:13 +0200

    DAG done, removed tests because there is nothing fragile to test

--- src/whirl/core.py
+++ src/whirl/core.py
@@ -1,29 +1,74 @@
 import sys
 import pathlib
 import yaml
+import networkx as nx
 
 
-def parse_dag(dag_path: pathlib.Path) -> tuple[bool, list[pathlib.Path]]:
+def parse_dag(dag_path: pathlib.Path) -> list[pathlib.Path]:
     with open(dag_path, "r") as file:
         raw_dag = yaml.load(file, Loader=yaml.Loader)
 
-    # TODO: Validate that DAG is actually acyclic and create output config
+    tasks = raw_dag["tasks"]
 
-    return (False, [])
+    G = nx.DiGraph()
+
+    task_all = all(["task" in x for x in tasks])
+
+    file_all = all(["file" in x for x in tasks])
+
+    if not task_all or not file_all:
+        print("DAG definition is invalid. All tasks must have task and file")
+        sys.exit(1)
+
+    G.add_nodes_from([x["task"] for x in tasks])
+
+    for t in tasks:
+        if "after" in t:
+            from_t = t["after"]
+            to_t = t["task"]
+
+            if isinstance(from_t, list):
+                for t in from_t:
+                    G.add_edge(from_t, to_t)
+            else:
+                G.add_edge(from_t, to_t)
+
+    acyclic = nx.is_directed_acyclic_graph(G)
+
+    tournament = nx.is_tournament(G)
+
+    if not acyclic and not tournament:
+        print("DAG error. Cycle found.")
+        sys.exit(1)
+
+    hamiltonian_path = nx.tournament.hamiltonian_path(G)
+    
+    lookup = {x["task"]: x["file"] for x in tasks}
+
+    run_order = [pathlib.Path(lookup[x]).absolute() for x in hamiltonian_path]
+
+    return run_order
 
 
 def run(args: dict) -> None:
+    command = args.command
     file_path = pathlib.Path(args.file).absolute()
-
+    
     if not file_path.exists():
         print("Invalid file path, try again")
         sys.exit(1)
 
-    valid, run_config = parse_dag(file_path)
+    run_config = parse_dag(file_path)
 
-    if not valid:
-        print("Invalid DAG, exiting...")
-        sys.exit(1)
+    if command == "plan":
+        print("Whirl will execute the notebooks in following order")
+        print("Order is based on Hamiltonian path in directed acyclic graph\n")
+        for idx,f in enumerate(run_config):
+            if not f.exists():
+                e_message = "ERROR: File on given path does not exist."
+                print(f"{idx+1} : {f} - {e_message}")
+            else:
+                print(f"{idx+1} : {f} - File OK")
 
 
 def generate() -> None:
